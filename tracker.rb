b08e8d351f59c3cc405651b4139e64f7c3393ebe
Zeroalpha <christian.moniatis@gmail.com>
2015-12-18 19:06:27 +0100

    First working pull-feature

--- tracker.rb
+++ tracker.rb
@@ -1,7 +1,37 @@
 require 'pry'
-require 'net/http'
 
-def pull_current_orderbook
-  url = URI("https://bitcoinapi.de/v1/YOUR_API_KEY/orderbook.json")
-  Net::HTTP.start()
+require 'net/http'
+require 'openssl'
+require 'json'
+
+class BitcoinAPI
+  
+  def initialize(api_key)
+    @API_KEY = api_key
+    @URLS = {
+      rate: URI('https://bitcoinapi.de/v1/' + @API_KEY + '/rate.json'),
+      orderbook: URI('https://bitcoinapi.de/v1/' + @API_KEY + '/orderbook.json')
+    }
+  end
+
+  def pull_data(type = :rate)
+    url = @URLS[type]
+    response = Net::HTTP.start(url.host,url.port, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+      request = Net::HTTP::Get.new url
+      http.request(request) 
+    end
+    JSON.load(response.body)    
+  end
+
+  def pull_current_orderbook
+    pull_data(:orderbook)
+  end
+
+  def pull_current_rate
+    pull_data(:rate)
+  end
 end
+
+api = BitcoinAPI.new File.read('api_key.sec')
+binding.pry
+puts ""
